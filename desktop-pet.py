e1c2a6d4329f802b621dd49538fb2ac9557280c4
Bryan Zeng <bzeng68@gmail.com>
2023-08-25 21:46:33 -0400

    Refactored desktop-pet.py logic to be cleaner and created draggabble.py to test draggable widget

--- desktop-pet.py
+++ desktop-pet.py
@@ -1,24 +1,7 @@
-import pyautogui
 import random
 import tkinter as tk
 import os
 import sys
-
-cycle = 0
-check = 1
-idle_num = [1, 2]
-heart_num = [6, 7]  # 5 is idle to sleep
-sleep_num = [10, 11, 12, 13, 15]  # 14 is sleep to idle
-walk_pos_num = [8, 9]
-walk_neg_num = [3, 4]
-event_number = random.randrange(1, 3, 1)
-
-window = tk.Tk()
-window_width = window.winfo_screenwidth()
-window_height = window.winfo_screenheight()
-
-x = window_width
-
 
 def resource_path(relative_path):
     base_path = getattr(
@@ -28,116 +11,90 @@ def resource_path(relative_path):
 
     return os.path.join(base_path, relative_path)
 
+def determine_action(low, high, x):
+    action_const = actions_list[random.randrange(low, high + 1, 1)]
+    next_action_range = possible_next_actions[action_const]
 
-idle = [tk.PhotoImage(file=resource_path('idle.gif'),
-                      format='gif -index %i' % (i)) for i in range(5)]
-idle_to_sleep = [tk.PhotoImage(
-    file=resource_path('idle_to_sleep.gif'), format='gif -index %i' % (i)) for i in range(5)]
-sleep = [tk.PhotoImage(file=resource_path('sleep.gif'),
-                       format='gif -index %i' % (i)) for i in range(4)]
-sleep_to_idle = [tk.PhotoImage(
-    file=resource_path('sleep_to_idle.gif'), format='gif -index %i' % (i)) for i in range(6)]
-heart = [tk.PhotoImage(
-    file=resource_path('heart.gif'), format='gif -index %i' % (i)) for i in range(4)]
-walk_positive = [tk.PhotoImage(file=resource_path('walking_positive.gif'),
-                               format='gif -index %i' % (i)) for i in range(6)]  # walk to left
-walk_negative = [tk.PhotoImage(file=resource_path('walking_negative.gif'),
-                               format='gif -index %i' % (i)) for i in range(6)]  # walk to right
+    movement_diff = 0
+    if action_const == WALK_POS_CONST:
+        movement_diff = -6
+    elif action_const == WALK_NEG_CONST:
+        movement_diff = 6
 
+    if not grabbed:
+        window.after(1, do_action, 0, action_const, movement_diff, x, next_action_range)
 
-def gif_work(cycle, frames, event_number, first_num, last_num):
-    if cycle < len(frames) - 1:
-        cycle = cycle + 1
-    else:
-        cycle = 0
-        event_number = random.randrange(first_num, last_num + 1, 1)
-    return cycle, event_number
+def do_action(curr_frame_num, action_const, movement_diff, x, next_action_range):
+    if not grabbed:
+        curr_frame = actions[action_const][curr_frame_num]
+        x += movement_diff
+        window.geometry('256x256+'+str(x - 286) +
+                        '+'+str(window_height - 316))
+        label.config(image = curr_frame)
+        label.pack()
 
-
-def update(cycle, check, event_number, x):
-    if check == 0:
-        frame = idle[cycle]
-        cycle, event_number = gif_work(cycle, idle, event_number, 1, 9)
-
-    elif check == 1:
-        frame = idle_to_sleep[cycle]
-        cycle, event_number = gif_work(
-            cycle, idle_to_sleep, event_number, 10, 10)
-
-    elif check == 2:
-        frame = sleep[cycle]
-        cycle, event_number = gif_work(
-            cycle, sleep, event_number, 10, 15)
-
-    elif check == 3:
-        frame = sleep_to_idle[cycle]
-        cycle, event_number = gif_work(
-            cycle, sleep_to_idle, event_number, 1, 1)
-
-    elif check == 4:
-        frame = heart[cycle]
-        cycle, event_number = gif_work(
-            cycle, heart, event_number, 1, 9)
-
-    elif check == 5:
-        frame = walk_positive[cycle]
-        cycle, event_number = gif_work(
-            cycle, walk_positive, event_number, 1, 9)
-        x -= 6
-
-    elif check == 6:
-        frame = walk_negative[cycle]
-        cycle, event_number = gif_work(
-            cycle, walk_negative, event_number, 1, 9)
-        x += 6
-    window.geometry('256x256+'+str(x - 286) +
-                    '+'+str(window_height - 316))
-    label.config(image=frame)
-    label.pack()
-    window.after(1, event, cycle, check, event_number, x)
-
-
-def event(cycle, check, event_number, x):
-    if event_number in idle_num:
-        check = 0
-        window.after(400, update, cycle, check, event_number, x)
-
-    elif event_number == 5:
-        check = 1
-        window.after(100, update, cycle, check, event_number, x)
-
-    elif event_number in heart_num:
-        check = 4
-        window.after(400, update, cycle, check, event_number, x)
-
-    elif event_number in sleep_num:
-        check = 2
-        window.after(1000, update, cycle, check, event_number, x)
-
-    elif event_number in walk_pos_num:
-        if x - 36 < 0:
-            check = 4
-            window.after(400, update, cycle, check, event_number, x)
+        wait_time = time_between_frame[action_const]
+        if curr_frame_num < len(actions[action_const]) - 1: # if action has more frames to go
+            window.after(wait_time, do_action, curr_frame_num + 1, action_const, movement_diff, x, next_action_range)
         else:
-            check = 5
-            window.after(500, update, cycle, check, event_number, x)
+            window.after(1, determine_action, next_action_range[0], next_action_range[1], x)
 
-    elif event_number in walk_neg_num:
-        if x + 292 > window_width:
-            check = 4
-            window.after(400, update, cycle, check, event_number, x)
-        else:
-            check = 6
-            window.after(500, update, cycle, check, event_number, x)
+grabbed = False
 
-    elif event_number == 14:
-        check = 3
-        window.after(100, update, cycle, check, event_number, x)
+IDLE_CONST = 'idle'
+ITS_CONST = 'idle_to_sleep'
+SLEEP_CONST = 'sleep'
+STI_CONST = 'sleep_to_idle'
+HEART_CONST = 'heart'
+WALK_POS_CONST = 'walk_positive'
+WALK_NEG_CONST = 'walk_negative'
 
+# list of actions with varying frequencies, used for determining which action
+# to do next. more frequent, more likely to happen
+actions_list = [IDLE_CONST, IDLE_CONST, # 0, 1
+                HEART_CONST, HEART_CONST, # 2, 3
+                WALK_POS_CONST, WALK_POS_CONST, # 4, 5
+                WALK_NEG_CONST, WALK_NEG_CONST, # 6, 7
+                ITS_CONST, # 8
+                SLEEP_CONST, SLEEP_CONST, SLEEP_CONST, SLEEP_CONST, SLEEP_CONST, # 9, 10, 11, 12, 13
+                STI_CONST] # 14
+
+# time between each frame for different actions
+time_between_frame = {IDLE_CONST: 500,
+                      HEART_CONST: 500,
+                      WALK_POS_CONST: 500,
+                      WALK_NEG_CONST: 500,
+                      ITS_CONST: 100,
+                      SLEEP_CONST: 1000,
+                      STI_CONST: 100}
+
+# range for possible nexxt action based on current action
+possible_next_actions = {IDLE_CONST: (0, 8),
+                         HEART_CONST: (0, 8),
+                         WALK_POS_CONST: (0, 8),
+                         WALK_NEG_CONST: (0, 8),
+                         ITS_CONST: (9, 9), # must sleep after sitting down
+                         SLEEP_CONST: (9, 14), # 80% to sleep, 20% to wake
+                         STI_CONST: (0, 0)} # must idle after getting up
+
+window = tk.Tk() # default window, the background
+window_width = window.winfo_screenwidth()
+window_height = window.winfo_screenheight()
+
+x = window_width
+
+# actions and their respective gifs
+actions = {IDLE_CONST: [tk.PhotoImage(file=resource_path('idle.gif'), format='gif -index %i' % (i)) for i in range(5)],
+           HEART_CONST: [tk.PhotoImage(file=resource_path('heart.gif'), format='gif -index %i' % (i)) for i in range(4)],
+           WALK_POS_CONST: [tk.PhotoImage(file=resource_path('walking_positive.gif'), format='gif -index %i' % (i)) for i in range(6)],  # walk to left
+           WALK_NEG_CONST: [tk.PhotoImage(file=resource_path('walking_negative.gif'), format='gif -index %i' % (i)) for i in range(6)],
+           ITS_CONST: [tk.PhotoImage(file=resource_path('idle_to_sleep.gif'), format='gif -index %i' % (i)) for i in range(5)],
+           SLEEP_CONST: [tk.PhotoImage(file=resource_path('sleep.gif'), format='gif -index %i' % (i)) for i in range(4)],
+           STI_CONST: [tk.PhotoImage(file=resource_path('sleep_to_idle.gif'), format='gif -index %i' % (i)) for i in range(6)]}  # walk to right
 
 if (os.name == "nt"):
     window.config(highlightbackground='black')
-    label = tk.Label(window, bg='black')
+    label = tk.Label(window, bg='black') # label is what the frame of the gif gets put on
     window.overrideredirect(True)
     window.wm_attributes('-transparentcolor', 'black')
     window.wm_attributes('-topmost', True)
@@ -148,6 +105,6 @@ else:
     window.wm_attributes('-topmost', True)
     window.wm_attributes('-transparent', True)
 
-window.after(1, update, cycle, check, event_number, x)
+window.after(1, determine_action, 0, 0, x)
 
 window.mainloop()
